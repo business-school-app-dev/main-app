1986e7315fb04237324df9ce75fdb4f41c4ab64f
juhic3162 <juhic3162@gmail.com>
2025-10-12 20:58:49 -0400

    added CourseRecScreen, no bugs

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import "@/global.css";
 import { Text, View } from "react-native";
+import CourseRecScreen from '../components/CourseRecScreen'; // adjust path if inside a folder
+
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-primary-500">
-        Welcome to Nativewind!!!
+        <CourseRecScreen/>
       </Text>
     </View>
   );

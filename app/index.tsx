90eb8ff85157798fef21f99017eb52e2100ffaf3
juhic3162 <juhic3162@gmail.com>
2025-10-18 15:30:28 -0400

    Bundling working for course-recommender screen. Have yet to implement gluestack components

--- app/index.tsx
+++ app/index.tsx
@@ -2,5 +2,5 @@ import { Redirect } from "expo-router";
 import "../global.css";
 
 export default function Index() {
-  return <Redirect href="/(tabs)/ui-demo" />
+  return <Redirect href="/(tabs)/ui-demo"/>
 }

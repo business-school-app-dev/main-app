158e5ce242fa1580e165eb25ba4900aaafdea004
gam3boy1 <aadiananddeveloper05@gmail.com>
2025-11-05 16:32:22 -0500

    fixed layout

--- app/(tabs)/quiz/_layout.tsx
+++ app/(tabs)/quiz/_layout.tsx
@@ -5,14 +5,11 @@ export default function Layout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen
         name="index"
-        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="quiz-modal"
         options={{
           presentation: 'fullScreenModal',
-          headerShown: false,
-          animation: 'ios_from_right',
         }}
       />
     </Stack>

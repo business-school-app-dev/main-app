bfddddf113fb0763f0e13994adf19880e6d56ca4
Juhi Chitkara <juhic3162@gmail.com>
2025-11-13 16:13:51 -0500

    fixed dropdowns for 2nd major and 2nd minor, added N/A option.

--- app/(tabs)/home/guides/course-recommender.tsx
+++ app/(tabs)/home/guides/course-recommender.tsx
@@ -42,7 +42,7 @@ const CourseRecommenderScreen = () => {
   const [comfortLevel, setComfortLevel] = useState('');
 
   const majors = [ "Accounting", "Agricultural and Resource Economics", "American Studies", "Animal and Avian Sciences", "Anthropology", "Applied Mathematics and Scientific Computation", "Arabic Studies", "Art History", "Astronomy", "Atmospheric and Oceanic Science", "Biochemistry", "Biological Sciences", "Chemistry", "Chinese Studies", "Cinema and Media Studies", "Civil Engineering", "Communication", "Computer Science", "Criminology and Criminal Justice", "Economics", "Electrical Engineering", "English Language and Literature", "Environmental Science and Policy", "Finance", "Geography", "Geology", "Government and Politics", "History", "Information Science", "International Business", "Journalism", "Linguistics", "Management", "Marketing", "Mathematics", "Mechanical Engineering", "Neuroscience", "Nursing", "Philosophy", "Physics", "Political Science", "Psychology", "Public Health Science", "Sociology", "Spanish Language and Literature", "Statistics", "Theatre", "Women's Studies" ]; 
-  const minors = [ "N/A", "African American Studies", "Arabic Studies", "Art History", "Asian American Studies", "Black Women\’s Studies", "Business Analytics", "Business Administration", "Chinese Studies", "Classics", "Communication", "Computer Science", "Creative Writing", "Criminology and Criminal Justice", "Dance", "Digital Studies", "East Asian Studies", "Economics", "Education", "English", "Environmental Science and Policy", "Film Studies", "French Studies", "Geographic Information Science", "German Studies", "Global Poverty", "History", "Human Development", "Information Science", "International Business", "Jewish Studies", "Journalism", "Latin American Studies", "Linguistics", "Management", "Marketing", "Philosophy", "Physics", "Political Science", "Psychology", "Public Health", "Sociology", "Spanish Studies", "Statistics", "Theatre", "Women\’s Studies" ];
+  const minors = [ "African American Studies", "Arabic Studies", "Art History", "Asian American Studies", "Black Women\’s Studies", "Business Analytics", "Business Administration", "Chinese Studies", "Classics", "Communication", "Computer Science", "Creative Writing", "Criminology and Criminal Justice", "Dance", "Digital Studies", "East Asian Studies", "Economics", "Education", "English", "Environmental Science and Policy", "Film Studies", "French Studies", "Geographic Information Science", "German Studies", "Global Poverty", "History", "Human Development", "Information Science", "International Business", "Jewish Studies", "Journalism", "Latin American Studies", "Linguistics", "Management", "Marketing", "Philosophy", "Physics", "Political Science", "Psychology", "Public Health", "Sociology", "Spanish Studies", "Statistics", "Theatre", "Women\’s Studies" ];
 
   const creditHours = ["0-29", "30-59", "60-89", "90-119", "120+"];
 
@@ -99,8 +99,8 @@ const CourseRecommenderScreen = () => {
 
             <FormSelect
               label="2nd Major"
-              placeholder="N/A"
-              options={majors}
+              placeholder="Select option"
+              options={["N/A", ...majors]}
               value={major2}
               onValueChange={setMajor2}
               isScrollable={true}
@@ -117,8 +117,8 @@ const CourseRecommenderScreen = () => {
 
             <FormSelect
               label="2nd Minor"
-              placeholder="N/A"
-              options={minors}
+              placeholder="Select option"
+              options={["N/A", ...minors]}
               value={minor2}
               onValueChange={setMinor2}
               isScrollable={true}
@@ -133,7 +133,8 @@ const CourseRecommenderScreen = () => {
             />
           </VStack>
 
-          {/* Submit Buttons */}
+          {/* Submit Buttons 
+            * TODO: add logic for only navigating if all dropdowns are filled, otw warning message*/}
           <View className="pt-4">
             <TextButton
               label="Get Course Recommendations"
